48cd24caba32ce971d43b799cb30dea8fa422e56
davisstacked <brettzvi@gmail.com>
2021-05-24 19:13:49 -0700

    login button component

--- src/Components/Auth/LoginButton.js
+++ src/Components/Auth/LoginButton.js
@@ -5,16 +5,16 @@ const LoginButton = (smallScreen) => {
   // if prop passed in, button will span entire width of container
   const width = smallScreen ? "w-full" : "";
   return (
-    <li>
-      <a
-        href="/login"
-        className={`${width} inline-flex items-center justify-center shadow-md bg-deep-purple-accent-400 h-12`}
-        aria-label="Login"
-        title="Login"
-      >
-        Login
-        <SignIn size={24} />
-      </a>
+    <li className="list-none">
+        <a
+          href="/login"
+          className={`${width} inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-5000 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none`}
+          aria-label="Login"
+          title="Login"
+        >
+          Login
+          <SignIn size={24} />
+        </a>
     </li>
   );
 };
